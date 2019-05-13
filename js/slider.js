ceec72510c54c48719029ffe56c7149c9a388cfe
choieh <bobomi66@naver.com>
2019-05-13 17:01:52 +0900

    slider.js
    slider.js

---
+++ js/slider.js
@@ -0,0 +1,14 @@
+    $(document).ready(function () {
+           var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      autoplay: {
+        delay: 3000,
+      },
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      }
+    });
+ 
+    });
