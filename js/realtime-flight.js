17e6249d181bd5df5fcfe6eebb899491aa870ee9
JHeeg <heeggggg@gmail.com>
2019-05-23 16:22:10 +0900

    realtime-flight _edit_0523

--- js/realtime-flight.js
+++ js/realtime-flight.js
@@ -27,27 +27,24 @@ $(document).ready(function() {
 			});
 		}
 	});
-	
-	//리스트 누르면 디테일 보여주기
-	$('.inner-flight-list > li .flight-info').on('click', function(e) {
+	//리스트 누르면 상세 보여주기
+	$('.inner-flight-list > .flight-info-box .flight-info').on('click', function(e) {
 		e.preventDefault();
-		var index = $('.inner-flight-list > li').index($(this).parent());
-		showDetail(index);
-	});
-	function showDetail(n) {
-		if ($('.inner-flight-list > li:eq(' + n + ')').hasClass('on')) {
-			$('.inner-flight-list > li:eq(' + n + ')').removeClass('on');
-			$('.inner-flight-list > li:eq(' + n + ') div.flight-detail').slideUp(500);
-			$('.flight-info:focus').blur();
+		var index = $('.inner-flight-list > .flight-info-box').index($(this).parent());
+		var detailHeight = $(this).parent().find('.flight-detail .flight-detail-box').outerHeight();
+		
+		if ($(this).parent().hasClass('on')) {
+			$(this).parent().removeClass('on');
+			$(this).parent().find('.flight-detail').stop().animate({
+				'height': 0
+			}, 1000);
 		} else {
-			$('.inner-flight-list > li').removeClass('on');
-			$('.inner-flight-list > li:eq(' + n + ')').addClass('on');
-			$('.inner-flight-list > li div.flight-detail').slideUp(500);
-			$('.inner-flight-list > li:eq(' + n + ') div.flight-detail').slideDown(500);
-			
+			$('.inner-flight-list > .flight-info-box').removeClass('on');
+			$(this).parent().addClass('on');
+			$(this).parent().find('.flight-detail').stop().animate({
+				'height': detailHeight
+			}, 1000);
 		}
-	}
-	
-	
+	});
 });
 
